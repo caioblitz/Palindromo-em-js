4298824028a07eb367a9b0dec7d0585a12ed00ad
caiob <caioblitz@gmail.com>
2022-05-23 15:40:10 -0300

    palindromo
    clear
    exit

--- scripts.js
+++ scripts.js
@@ -9,7 +9,12 @@ function verificaPalindromo (string) {
 function verificaPalindromo2(string) {
     if (!string) return "string inexistente";
 
-    for(let i = 0; i < string.length; i++) {
-        
+    for(let i = 0; i < string.length / 2; i++) {
+        if (string[i] !== [string.length -1 -i]){ 
+        return false;
     }
 }
+    return true;
+}
+
+console.log(verificaPalindromo2("abba"));
