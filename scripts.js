449d876073e8b9eb50a7d598f847e137c0e5538a
caiob <caioblitz@gmail.com>
2022-05-23 15:40:32 -0300

    palindromo2ok

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ function verificaPalindromo2(string) {
     if (!string) return "string inexistente";
 
     for(let i = 0; i < string.length / 2; i++) {
-        if (string[i] !== [string.length -1 -i]){ 
+        if (string[i] !== string [string.length -1 -i]){ 
         return false;
     }
 }
